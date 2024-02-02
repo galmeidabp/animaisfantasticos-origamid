78ee8afe9e1822d804768a8d8fe577651ab23ba3
Gabriella Almeida <galmeidabp@gmail.com>
2024-02-02 17:00:06 -0300

    scroll

--- script.js
+++ script.js
@@ -39,3 +39,24 @@ function initAccordion() {
   }
 }
 initAccordion()
+
+function initScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"')
+
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute("href")
+    const section = document.querySelector(href)
+    const topo = section.offsetTop
+
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection)
+  })
+}
+initScroll()
