5fe1152b580eb9f17b8e057bc4afd24d495c505e
Gabriella Almeida <galmeidabp@gmail.com>
2024-02-21 19:11:36 -0300

    dataset

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll(".js-tabmenu li")
-  const tabContent = document.querySelectorAll(".js-tabcontent section")
+  const tabMenu = document.querySelectorAll("[data-tab='menu'] li")
+  const tabContent = document.querySelectorAll("[data-tab='content'] section")
 
   if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add("ativo")
@@ -9,7 +9,8 @@ function initTabNav() {
       tabContent.forEach((section) => {
         section.classList.remove("ativo")
       })
-      tabContent[index].classList.add("ativo")
+      const direcao = tabContent[index].dataset.anime
+      tabContent[index].classList.add("ativo", direcao)
     }
 
     tabMenu.forEach((itemMenu, index) => {
@@ -22,7 +23,7 @@ function initTabNav() {
 initTabNav()
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll(".js-accordion dt")
+  const accordionList = document.querySelectorAll("[data-anime='accordion'] dt")
   const activeClass = "ativo"
   if (accordionList.length) {
     accordionList[0].classList.add(activeClass)
@@ -41,7 +42,9 @@ function initAccordion() {
 initAccordion()
 
 function initScroll() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"')
+  const linksInternos = document.querySelectorAll(
+    '[data-menu="suave"] a[href^="#"'
+  )
 
   function scrollToSection(event) {
     event.preventDefault()
@@ -62,7 +65,7 @@ function initScroll() {
 initScroll()
 
 function initAnimationScroll() {
-  const sections = document.querySelectorAll(".js-scroll")
+  const sections = document.querySelectorAll("[data-anime='scroll']")
 
   if (sections.length) {
     const windowMetade = window.innerHeight * 0.6
