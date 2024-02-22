043c348f793c83c6018e4f7182df0152f3a8b8f1
Gabriella Almeida <galmeidabp@gmail.com>
2024-02-22 10:33:31 -0300

    modules

---
+++ js/modules/scroll-suave.js
@@ -0,0 +1,21 @@
+export default function initScroll() {
+  const linksInternos = document.querySelectorAll(
+    '[data-menu="suave"] a[href^="#"'
+  )
+
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute("href")
+    const section = document.querySelector(href)
+    const topo = section.offsetTop
+
+    window.scrollTo({
+      top: topo,
+      behavior: "smooth",
+    })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection)
+  })
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initTabNav from "./modules/tabnav.js"
+import initAccordion from "./modules/accordion.js"
+import initScroll from "./modules/scroll-suave.js"
+import initAnimationScroll from "./modules/scroll-animacao.js"
+
+initTabNav()
+initAccordion()
+initScroll()
+initAnimationScroll()
