b6ee3bd61fd0263c8352847f077ff0def7c401d6
Gabriella Almeida <galmeidabp@gmail.com>
2024-02-22 11:42:31 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initTabNav from "./modules/tabnav.js"
 import initAccordion from "./modules/accordion.js"
 import initScroll from "./modules/scroll-suave.js"
 import initAnimationScroll from "./modules/scroll-animacao.js"
+import initModal from "./modules/modal.js"
 
 initTabNav()
 initAccordion()
 initScroll()
 initAnimationScroll()
+initModal()
