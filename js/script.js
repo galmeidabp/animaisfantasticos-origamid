4fd8e258a87dfe24f957e24e11684aebfc9d66d0
Gabriella Almeida <galmeidabp@gmail.com>
2024-02-27 11:06:03 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordion from "./modules/accordion.js"
 import initScroll from "./modules/scroll-suave.js"
 import initAnimationScroll from "./modules/scroll-animacao.js"
 import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tooltip.js"
 
 initTabNav()
 initAccordion()
 initScroll()
 initAnimationScroll()
 initModal()
+initTooltip()
