69e0e35471d4016cbc0c090d7a7df1a41946f25f
Gabriella Almeida <galmeidabp@gmail.com>
2024-03-13 17:55:57 -0300

    mobile-menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -14,25 +14,4 @@ export default function initDropdownMenu() {
       this.classList.remove("active")
     })
   }
-
-  function outsideClick(element, events, callback) {
-    const html = document.documentElement
-    const outside = "data-outside"
-
-    if (!element.hasAttribute(outside)) {
-      events.forEach((userEvent) => {
-        html.addEventListener(userEvent, handleOutsideClick)
-      })
-      element.setAttribute(outside, "")
-    }
-    function handleOutsideClick(event) {
-      if (!element.contains(event.target)) {
-        element.removeAttribute(outside)
-        events.forEach((userEvent) => {
-          html.removeEventListener(userEvent, handleOutsideClick)
-        })
-        callback()
-      }
-    }
-  }
 }
--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ import initScroll from "./modules/scroll-suave.js"
 import initAnimationScroll from "./modules/scroll-animacao.js"
 import initModal from "./modules/modal.js"
 import initTooltip from "./modules/tooltip.js"
+
 import initDropdownMenu from "./modules/dropdown-menu.js"
+import initMenuMobile from "./modules/menu-mobile.js"
 
 initTabNav()
 initAccordion()
@@ -12,4 +14,6 @@ initScroll()
 initAnimationScroll()
 initModal()
 initTooltip()
+
 initDropdownMenu()
+initMenuMobile()
